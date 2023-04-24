02ab89df6cac3e6c6d20ab7377ac465cc1ec0143
Rstur <4382016@qq.com>
2023-04-24 10:31:29 +0800

    29

--- 100 Exercises/Ex29.c
+++ 100 Exercises/Ex29.c
@@ -1,3 +1,30 @@
 /*
 请编写一个函数fun，它的功能是：将ss所指字符串中所有下标为奇数位置上的字母转换为大写(若该位置上不是字母，则不转换)。
 */
+#include <stdio.h>
+#include <string.h>
+
+void fun(char *ch)
+{
+    int len;
+    len = strlen(ch);
+    for (int i = 0; i <= len; i++)
+    {
+        if ((i % 2 == 0) && (ch[i] >= 'a') && (ch[i] <= 'z'))
+        {
+            ch[i] = ch[i] - 32; // 大写字母和小写字母之间ASCII码差值为32
+        }
+    }
+}
+
+int main(void)
+{
+
+    char ch[50];
+    printf("Enter words:");
+    scanf("%s", ch);
+    fun(ch);
+    printf("%s", ch);
+
+    return 0;
+}
