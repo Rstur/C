f9d25deaf5040c4183e76589c76bab8bd8dd821b
Rstur <4382016@qq.com>
2023-04-24 13:42:59 +0800

    31

---
+++ 100 Exercises/Ex31.c
@@ -0,0 +1,34 @@
+/*
+请编写函数fun，其功能是：
+将s所指字符串中除了下标为偶数、同时ASCII值也为偶数的字符外，其余的全都删除；串中剩余字符所形成的一个新串放在t所指的一个数组中。
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+int fun(char *s, char *t)
+{
+    int i, j = 0;
+    int len = strlen(s);
+    for (i = 0; i < len; i++)
+    {
+        if ((i % 2 == 0) && (s[i] % 2 == 0))
+        {
+            t[j++] = s[i];
+        }
+    }
+    printf("%s", t);
+
+    return 0;
+}
+
+int main(void)
+{
+    char s[50];
+    char t[50];
+    printf("Enter words:");
+    scanf("%s", s);
+    fun(s, t);
+
+    return 0;
+}
