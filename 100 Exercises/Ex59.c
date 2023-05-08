b43cbcf330ebee606a4c687fad908fdbb09296a3
Rstur <4382016@qq.com>
2023-05-08 14:23:47 +0800

    59,60

--- 100 Exercises/Ex59.c
+++ 100 Exercises/Ex59.c
@@ -2,3 +2,32 @@
 请别写函数fun，该函数的功能是：将放在字符串数组中的M个字符串(每串的长度不超过N)，按顺序合并组成一个新的字符串
 */
 
+#include<stdio.h>
+#include<string.h>
+#define N 100
+
+void fun();
+
+int main(void)
+{
+    char str[][N] = { "hello","world","are","you","ok?","no" };
+    int n = 0;
+
+    printf("Enter a number:\n");
+    scanf("%d", &n);
+    char str2[N];
+    fun(str, str2, n);
+    printf("%s", str2);
+
+    return 0;
+}
+
+void fun(char str[][N], char str2[], int n)
+{
+    int count = 0;
+    for (int i = 0;i < n;i++)
+    {
+        for (int j = 0;j < strlen(str[i]);j++)
+            str2[count++] = str[i][j];
+    }
+}
--- 100 Exercises/Ex60.c
+++ 100 Exercises/Ex60.c
@@ -2,3 +2,31 @@
 请编写函数fun，该函数的功能是：删去一维数组中所有相同的数，使之只剩一个。
 数组中的数已按由小到大的顺序排列，函数返回删除后数组中数据的个数。
 */
+
+#include<stdio.h>
+
+int fun();
+
+int main(void)
+{
+    int num[] = { 12,12,12,12,13,13,13,14,14,14,15,15,15,16,16,16,17,17,18 };
+    int len = fun(num, sizeof(num) / sizeof(num[0]));
+    for (int i = 0; i < len; i++)
+    {
+        printf("%2d ", num[i]);
+    }
+    return 0;
+}
+
+int fun(int num[], int k)
+{
+
+    int i, j = 1;
+    for (i = 1;i < k;i++)
+        if (num[j - 1] != num[i])
+        {
+            num[j] = num[i];
+            j++;
+        }
+    return j;
+}
