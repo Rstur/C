ef1d7e3d0d766f06e7b4d09e389d8b92535d3058
Rstur <4382016@qq.com>
2023-05-09 10:55:22 +0800

    62

--- 100 Exercises/Ex62.c
+++ 100 Exercises/Ex62.c
@@ -2,3 +2,34 @@
 请编写函数fun，该函数的功能是：统一一含字符串中单词的个数，作为函数值返回。
 一行字符串在主函数中输入，规定所有单词由小写字母组成，单词之间由若干个空格格开，一行的开始没有空格。
 */
+
+#include<stdio.h>
+
+int fun();
+
+int main(void)
+{
+    char str[100];
+    printf("Enter a string:");
+    fgets(str, 100, stdin);
+
+    int count = fun(str);
+    printf("The number of words in string is %d", count);
+
+    return 0;
+}
+
+int fun(char* s)
+{
+    int count = 1;
+    while (*s)
+    {
+        if (*s == ' ')
+        {
+            count++;
+            s++;
+        }
+        s++;
+    }
+    return count;
+}
